cdfc4f7220c102c5cab9cf432ee688da254bb14d
surmount96 <ayanwoye74@gmail.com>
2021-11-06 22:24:43 +0100

    bug fixes

--- src/views/public/index/index.vue
+++ src/views/public/index/index.vue
@@ -2,42 +2,12 @@
   <div class="home relative">
     <div class="w-full flex">
       <!-- Contains filters -->
-      <div class="w-1/5 p-10 sticky top-0" style="height:89vh">
-        <!-- category area -->
-        <div>
-          <h2 class="font-bold mb-4">Category</h2>
-
-          <!-- display list of all categories -->
-          <div
-            v-for="category in categories"
-            :key="category"
-            class="mb-4 font-14"
-          >
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-            <label for="vehicle1" class="ml-2" style="color:#868e96">{{
-              category
-            }}</label>
-          </div>
-        </div>
-
-        <!-- trends area -->
-        <div class="mt-8">
-          <h2 class="font-bold mb-4">Trends</h2>
-
-          <!-- display list of all trends -->
-          <div v-for="trend in trends" :key="trend" class="mb-4 font-14">
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-            <label for="vehicle1" class="ml-2" style="color:#868e96">{{
-              trend
-            }}</label>
-          </div>
-        </div>
-      </div>
+      
 
       <Loader v-if="loading" loadingText="Getting Products..." />
 
       <!-- contains content -->
-      <div v-else class="w-4/5 px-10 pt-10 pb-40">
+      <div v-else class="px-10 pt-10 pb-40">
         <div class="grid grid-cols-3 gap-4">
           <!-- Image component -->
           <SingleProduct
