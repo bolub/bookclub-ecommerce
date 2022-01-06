2217d48a4bd3e99639a2d127f8e6155d39ff2c43
surmount96 <ayanwoye74@gmail.com>
2022-01-06 03:56:41 +0100

    navogation correction

--- src/components/ui/Navbar.vue
+++ src/components/ui/Navbar.vue
@@ -186,10 +186,10 @@
                     <a href="https://app.bookclubwithlove.org/register" target="_blank" class="bg-gray-300 py-3 w-32 flex items-center justify-center text-medium rounded">Get started</a>
                 </li>
                 <li class="ml-8">
-                    <a href="https://sandbox.bookclubwithlove.org/live-stream" class="text-red font-bold flex items-center border border-red py-2 px-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <a href="https://sandbox.bookclubwithlove.org/live-stream" class="text-red font-bold flex items-center py-2 px-3">
+                        <!-- svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
-                        </svg>
+                        </!-->
                         Join a Session
                     </a>
                 </li>
